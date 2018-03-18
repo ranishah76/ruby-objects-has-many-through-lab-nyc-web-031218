e7a19d0a5f92b8e0c168ddd5ab0dc9f28afdb963
Rani Shah <rani76@gmail.com>
2018-03-18 18:13:05 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,10 +1,23 @@
 class Patient
 
-attr_reader :name 
+attr_reader :name
 
 def initialize(name)
   @name = name
   @appointments = []
 end
+
+def add_appointment(appointment)
+  @appointments << apointment
+  appointment.patient = self 
+end 
+
+def appointments 
+  @appointments
+end 
+
+def doctors 
+  
+
 
 end
