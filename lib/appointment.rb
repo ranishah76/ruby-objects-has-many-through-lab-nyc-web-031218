cdf4873b8320734d80af7d14bb4364ca468fa556
Rani Shah <rani76@gmail.com>
2018-03-18 18:08:27 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,13 +1,12 @@
-class Appointment 
+class Appointment
 
-  attr_accessor :name, :doctor 
+  attr_accessor :name, :doctor
 
   def initialize(name, doctor)
-    @name = name 
-    @doctor = doctor 
-  end 
+    @name = name
+    @doctor = doctor
+  end
 
   
 
-end 
-
+end
